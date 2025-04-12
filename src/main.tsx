a574c8af3a6b7584d2fa961f2a0eb8d8d108c267
Himal Shakya <shakyahimal1@gmail.com>
2025-04-12 02:06:06 -0400

    Added a function

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-// import { Authenticator } from '@aws-amplify/ui-react';
-import MyAuthenticator from "./components/MyAuthenticator.tsx"
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MyAuthenticator>
+    <Authenticator>
     <App />
-    </MyAuthenticator>
+    </Authenticator>
   </React.StrictMode>
 );
